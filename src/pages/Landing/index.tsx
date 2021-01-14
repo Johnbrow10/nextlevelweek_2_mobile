5954b5d61d148d3bc4f7f0a05fc868f5b42872fb
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-14 18:23:18 -0300

    Adicionando as fonts e o estilo da pagina Landing

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+import landingImg from '../../assets/images/landing.png';
+
+export default function Landing() {
+    return (
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner} />
+
+            <Text style={styles.title}> 
+                Seja Bem vindo, {'\n'}
+                <Text style={styles.titleBold}> O que deseja Fazer? </Text>
+            </Text>
+        </View>
+    );
+}
