4a32337d3a7844f4da650da62c22ee33fe85b731
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-14 20:06:09 -0300

    Adicionando botões na Landing e estilizando

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity, } from 'react-native';
 
 import styles from './styles';
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 
 export default function Landing() {
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
 
-            <Text style={styles.title}> 
+            <Text style={styles.title}>
                 Seja Bem vindo, {'\n'}
                 <Text style={styles.titleBold}> O que deseja Fazer? </Text>
             </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    < Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    < Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
+
+
+
     );
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -7,20 +7,54 @@ const styles = StyleSheet.create({
         padding: 40,
         justifyContent: 'center',
     },
+
     banner: {
         width: '100%',
         resizeMode: 'contain',
     },
-    title : {
+
+    title: {
         fontFamily: 'Poppins_400Regular',
         color: '#fff',
         fontSize: 20,
         lineHeight: 30,
         marginTop: 80,
     },
+
     titleBold: {
-       fontFamily: 'Poppins_600SemiBold',
+        fontFamily: 'Poppins_600SemiBold',
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between',
+    },
+
+    button: {
+        height: 150,
+        width: '48%',
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between',
+
+    },
+
+    buttonPrimary: {
+        backgroundColor: '#9871f5',
+    },
+
+    buttonSecondary: {
+        backgroundColor: '#04d361',
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 20,
     }
+
 });
 
 export default styles;
