81936b1b9d47b397a50ea7801e0f987dc722eada
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-15 15:48:41 -0300

    Add o Rect botao, com stack levando ate outra page

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -3,14 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function AppStack() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown:false}}>
+            <Navigator screenOptions={{ headerShown: false }}>
                 <Screen name="Landing" component={Landing} />
+                <Screen name="GiveClasses" component={GiveClasses} />
+
             </Navigator>
         </NavigationContainer>
     )
