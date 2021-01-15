f8e1f45ca04fdbae6bcd8c308706a3c9cb5017ae
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-15 17:26:19 -0300

    Função  goBack no botão para voltar pagina inicial

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -3,8 +3,15 @@ import { View, ImageBackground, Text } from 'react-native';
 import styles from '../GiveClasses/styles';
 import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export default function GiveClasses() {
+
+    const { goBack } = useNavigation();
+    function handleNavigateBack() {
+        goBack();
+    }
+
     return (
         <View style={styles.container}>
             <ImageBackground
@@ -16,7 +23,7 @@ export default function GiveClasses() {
                     Para começar, você precisa se cadastrar como um professor na nossa plataforma web.
                 </Text>
             </ImageBackground>
-            <RectButton style={styles.okButton}>
+            <RectButton onPress={handleNavigateBack} style={styles.okButton}>
                 <Text style={styles.okButtonText}>Tudo Bem</Text>
             </RectButton>
         </View>
