250e3e75fe05bbe5a11a3ee4fea41b3e7092854a
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-15 16:01:19 -0300

    Add uma imagem no background na page GiveClasses

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { View, ImageBackground } from 'react-native';
+import styles from '../GiveClasses/styles';
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 export default function GiveClasses() {
     return (
-        <View></View>
-    )
+        <View style={styles.container}>
+            <ImageBackground resizeMode='contain' source={giveClassesBgImage} style={styles.content} >
+
+            </ImageBackground>
+        </View>
+    );
 }
---
+++ src/pages/GiveClasses/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#8257E5',
+        flex: 1,
+        padding: 40,
+        justifyContent: 'center',
+    },
+
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+});
+
+export default styles;
