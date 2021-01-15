47f655708102f2a94913e21838329c871cb27c4a
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-15 17:13:26 -0300

    Ajustando o Texto na Tela de Give Classes.

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, ImageBackground } from 'react-native';
+import { View, ImageBackground,Text } from 'react-native';
 import styles from '../GiveClasses/styles';
 import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 export default function GiveClasses() {
     return (
         <View style={styles.container}>
-            <ImageBackground resizeMode='contain' source={giveClassesBgImage} style={styles.content} >
-
+            <ImageBackground 
+            resizeMode='contain' 
+            source={giveClassesBgImage} 
+            style={styles.content} >
+                <Text style={styles.title}>Quer ser um Proffy?</Text>
+                <Text style={styles.description}>
+                    Para começar, você precisa se cadastrar como um professor na nossa plataforma web.
+                </Text>
             </ImageBackground>
+
         </View>
     );
 }
--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -1,3 +1,4 @@
+import { Archivo_700Bold } from '@expo-google-fonts/archivo';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -11,6 +12,21 @@ const styles = StyleSheet.create({
     content: {
         flex: 1,
         justifyContent: 'center',
+    },
+    title: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180,
+    },
+    description: {
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 24,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240,
     },
 });
 
