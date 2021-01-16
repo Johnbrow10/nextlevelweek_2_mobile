aacfa045cf67ad605420dcb390684fef2ee9ba29
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-15 21:55:28 -0300

    tabs levando as pages favorites e TeacherList. E estilizando essas paginas com um component Header, e criando botao no Header para voltar a  LandingPage.

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/pageHeader';
 
 import styles from './styles';
 
 export default function Favorites() {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <View style={styles.container}>
+
+                <PageHeader title="Meus proffys favoritos" />
+            </View>
+        </View>
     );
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/pageHeader';
 import styles from './styles';
 
 
 export default function TeacherList() {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+
+            <PageHeader title="Proffys Disponíveis"/>
+        </View>
     )
 }
