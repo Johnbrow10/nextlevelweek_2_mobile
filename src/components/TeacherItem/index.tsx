a126eb172596dc122f8a2e22d7f223d51c6d3c90
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-19 16:32:36 -0300

    fotter dos dados da listagem dos professores. com botoes para entrar em contato e add aos favoritos. Com estilização ja pronta

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsAppIcon from '../../assets/images/icons/whatsapp.png';
 
 export default function TeacherItem() {
     return (
@@ -23,6 +27,24 @@ export default function TeacherItem() {
                 Vidrado em todo tipo de historia desde os pre historicos homo sapiens desbravando e lutando
                  por regiões com os nerdentais as incriveis tecnologias feitas pelo homem.
             </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'   '}
+                    <Text style={styles.priceValue}> R$ 20,00 </Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon} />
+                    </RectButton>
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsAppIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em Contato</Text>
+                    </RectButton>
+                </View>
+
+            </View>
         </View>
     );
 }
