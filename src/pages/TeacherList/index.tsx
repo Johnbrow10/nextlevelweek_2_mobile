085ac0352b2682de8f6998a8036cfab1805f2d13
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2021-01-19 20:11:44 -0300

    colocando um useState com o padrao false.  esconder os input de filtro e depois irá existir botao mudar o estate.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import PageHeader from '../../components/pageHeader';
 import TeacherItem from '../../components/TeacherItem';
@@ -6,41 +6,44 @@ import styles from './styles';
 
 
 export default function TeacherList() {
+
+    const [isFilterVisible, setIsFilterVisible] = useState(false);
     return (
         <View style={styles.container}>
 
             <PageHeader title="Proffys Disponíveis">
-                <View style={styles.searchForm}>
-                    <Text style={styles.label}> Matéria</Text>
+                {isFilterVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}> Matéria</Text>
 
-                    <TextInput
-                        style={styles.input}
-                        placeholderTextColor="#c1bccc"
-                        placeholder="Qual a Matéria?"
-                    ></TextInput>
+                        <TextInput
+                            style={styles.input}
+                            placeholderTextColor="#c1bccc"
+                            placeholder="Qual a Matéria?"
+                        ></TextInput>
 
-                    <View style={styles.inputGroup}>
-                        <View style={styles.inputBlock}>
-                            <Text style={styles.label}> Dia da Semana</Text>
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}> Dia da Semana</Text>
 
-                            <TextInput
-                                style={styles.input}
-                                placeholderTextColor="#c1bccc"
-                                placeholder="Qual o dia?"
-                            ></TextInput>
-                        </View>
-                        <View style={styles.inputBlock}>
-                            <Text style={styles.label}> Horário</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholderTextColor="#c1bccc"
+                                    placeholder="Qual o dia?"
+                                ></TextInput>
+                            </View>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}> Horário</Text>
 
-                            <TextInput
-                                style={styles.input}
-                                placeholderTextColor="#c1bccc"
-                                placeholder="Qual Horário?"
-                            ></TextInput>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholderTextColor="#c1bccc"
+                                    placeholder="Qual Horário?"
+                                ></TextInput>
+                            </View>
                         </View>
                     </View>
-                </View>
-
+                )}
             </PageHeader>
             <ScrollView
                 style={styles.teacherList}
